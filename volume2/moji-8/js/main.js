d0af04c36305678ae0808b995d67f2c398d0fbbb
raddevus <1radeutsch1@gmail.com>
2021-02-20 23:03:48 -0500

    Added load event handler to insure the attachEmoji code runs automatically.

---
+++ volume2/moji-8/js/main.js
@@ -0,0 +1,17 @@
+addEventListener("load",attachEmojiClickListeners);
+
+function attachEmojiClickListeners(){
+    console.log("in attachEmojiClickListeners...");
+    // First get the collection of Nodes that we want to work with
+    // and store the collection in a variable for later use
+    let allNodes = document.querySelectorAll(".emoji span");
+
+    // Next, use a for loop to iterate over each Node in the collection
+    // The NodeList provides a property named length to determine
+    // how many nodes are in the list and we use that now.
+    for (let i = 0; i < allNodes.length; i++){
+        allNodes[i].addEventListener("click", function() {
+            alert(allNodes[i].innerHTML);
+        }); // end of addEventListener function
+    } // end of For loop
+} // end of function
